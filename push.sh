587e3130b613ce044494647ab4873070c6dce2ec
root <daniel.farrell2626@gmail.com>
2022-09-06 20:25:50 +0000

    No message

---
+++ push.sh
@@ -0,0 +1,5 @@
+
+               git add . 
+               git commit -m 'No message' 
+               git push https://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@github.com/farrell0/My_KG_NoteBooks.git
+               
