c292cd73b231248b247d4300f6f96396bbf5d149
farrell0 <farrell0@users.noreply.github.com>
2022-09-06 13:26:22 -0700

    Delete push.sh

--- push.sh
+++
@@ -1,5 +0,0 @@
-
-               git add . 
-               git commit -m 'No message' 
-               git push https://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@github.com/farrell0/My_KG_NoteBooks.git
-               
