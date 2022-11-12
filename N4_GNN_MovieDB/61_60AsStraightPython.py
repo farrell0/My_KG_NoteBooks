9a6c707ff833318a6051003d0a63b4774a4fc46e
Push.sh <Push.sh@example.com>
2022-11-12 17:53:17 +0000

    No message

---
+++ N4_GNN_MovieDB/61_60AsStraightPython.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+DB_NAME         = "my_db"
+GRAPH_NAME      = "my_graph"
+   #
+my_graph, *_ = my_client.get_database(name=DB_NAME).find_graphs_by_name(GRAPH_NAME)
+   #
+print(my_graph)
+
+
+display("Number of Graph Nodes: %d" % (my_graph.num_nodes()))
+display("Number of Graph Edges: %s" % (my_graph.num_edges()))
+
