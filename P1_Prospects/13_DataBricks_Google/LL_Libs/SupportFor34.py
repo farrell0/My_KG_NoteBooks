e2866faa245172d0dadf9db45ae011ed2e82780c
Push.sh <Push.sh@example.com>
2022-12-27 21:04:09 +0000

    No message

--- P1_Prospects/13_DataBricks_Google/LL_Libs/SupportFor34.py
+++ P1_Prospects/13_DataBricks_Google/LL_Libs/SupportFor34.py
@@ -1,68 +1,68 @@
 
 
-# #############################################################
+#  # #############################################################
 
 
-def f_init1():
-    
-   import pandas as pd
-   from tabulate import tabulate
+#  def f_init1():
+#      
+#     import pandas as pd
+#     from tabulate import tabulate
 
 
-   pd.set_option("display.width", 480)
-   
-   #  Sets horizontal scroll for wide outputs
-   #
-   from IPython.display import display, HTML
-   display(HTML(""))
+#     pd.set_option("display.width", 480)
+#     
+#     #  Sets horizontal scroll for wide outputs
+#     #
+#     from IPython.display import display, HTML
+#     display(HTML(""))
 
-   return 
-
-
-# #############################################################
+#     return 
 
 
-def f_init2():
-    
-   from katana import remote
+#  # #############################################################
 
 
-   NUM_PARTITIONS  = 3
-      #
-   DB_NAME         = "my_db"
-   GRAPH_NAME      = "my_graph"
-   
-    
-   #  Get a client handle
-   #
-   my_client = remote.Client()
-   
-   #  Connect to graph
-   #
-   my_graph, *_ = my_client.get_database(name=DB_NAME).find_graphs_by_name(GRAPH_NAME)
-       
-    
-   return my_client, my_graph
+#  def f_init2():
+#      
+#     from katana import remote
 
 
-# #############################################################
+#     NUM_PARTITIONS  = 3
+#        #
+#     DB_NAME         = "my_db"
+#     GRAPH_NAME      = "my_graph"
+#     
+#      
+#     #  Get a client handle
+#     #
+#     my_client = remote.Client()
+#     
+#     #  Connect to graph
+#     #
+#     my_graph, *_ = my_client.get_database(name=DB_NAME).find_graphs_by_name(GRAPH_NAME)
+#         
+#      
+#     return my_client, my_graph
 
 
-def f_init():
-    
-#  f_init1()
-
-#  my_client, my_graph = f_init2()
-
-   print("rrr")
-    
-   sss = 34
-    
-    
-   return  (sss)
+#  # #############################################################
 
 
+#  def f_init():
+#      
+#  #  f_init1()
 
-def bbb():
-    
-   return 34
+#  #  my_client, my_graph = f_init2()
+
+#     print("rrr")
+#      
+#     sss = 34
+#      
+#      
+#     return  (sss)
+
+
+
+#  def bbb():
+#      
+#     return 34
