f7bfde89b20fe7da4464118fc8c577375d8e4ec7
Push.sh <Push.sh@example.com>
2022-12-27 18:44:19 +0000

    No message

---
+++ P1_Prospects/13_DataBricks_Google/04_Libs/35_SupportFor34.py
@@ -0,0 +1,21 @@
+
+
+import pandas as pd
+from tabulate import tabulate
+
+
+def f_init()
+
+
+   pd.set_option("display.width", 480)
+   
+   #  Sets horizontal scroll for wide outputs
+   #
+   from IPython.display import display, HTML
+   display(HTML(""))
+
+
+   NUM_PARTITIONS  = 3
+      #
+   DB_NAME         = "my_db"
+   GRAPH_NAME      = "my_graph"
