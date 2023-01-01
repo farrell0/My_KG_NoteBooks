ccb97bdde22b5782b18c89f929bdfa5be107bf83
Push.sh <Push.sh@example.com>
2023-01-01 23:52:59 +0000

    No message

--- P1_Prospects/13_DataBricks_Google/libraries/SupportFor34.py
+++ P1_Prospects/13_DataBricks_Google/libraries/SupportFor34.py
@@ -215,7 +215,7 @@ def f_ready_for_graph_int(i_arg1, i_arg2):
                #
                #  We have an additional key, add to the record and add to our array
                #
-               l_recd1.update( {"preferred_term": l_entity["preferredTerm"]} )               #  Comment this out, 
+               l_recd1.update( {"preferred_term": l_entity["preferredTerm"]} )                 #  Comment this out, 
                   #
             l_UmlsEntityNodes.append(l_recd1)
             #
