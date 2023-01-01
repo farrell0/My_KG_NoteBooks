37de39a113528d8a85dc6eb657ff8372fe43fb74
Push.sh <Push.sh@example.com>
2023-01-01 22:40:33 +0000

    No message

--- P1_Prospects/13_DataBricks_Google/libraries/SupportFor34.py
+++ P1_Prospects/13_DataBricks_Google/libraries/SupportFor34.py
@@ -283,18 +283,18 @@ def f_insert_into_graph(i_arg1, i_arg2, i_arg3, i_arg4, i_arg5, i_arg6, i_arg7, 
          id_space              = "PatientVisit",  
          label                 = "PatientVisit",  
          ) 
-      df_importer.nodes_dataframe(
-         i_arg3,
-         id_column             = "id",
-         id_space              = "UmlsEntity",  
-         label                 = "UmlsEntity",  
-         ) 
-      df_importer.nodes_dataframe(
-         i_arg4,
-         id_column             = "id",
-         id_space              = "UmlsVocabulary",  
-         label                 = "UmlsVocabulary",  
-         ) 
+#     df_importer.nodes_dataframe(
+#        i_arg3,
+#        id_column             = "id",
+#        id_space              = "UmlsEntity",  
+#        label                 = "UmlsEntity",  
+#        ) 
+#     df_importer.nodes_dataframe(
+#        i_arg4,
+#        id_column             = "id",
+#        id_space              = "UmlsVocabulary",  
+#        label                 = "UmlsVocabulary",  
+#        ) 
 
       #  Just edges
       #
