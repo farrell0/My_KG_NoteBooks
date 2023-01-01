0af1cb5d80b5ad864f1a2f0eddd14f8054790831
Push.sh <Push.sh@example.com>
2023-01-01 22:32:28 +0000

    No message

--- P1_Prospects/13_DataBricks_Google/libraries/SupportFor34.py
+++ P1_Prospects/13_DataBricks_Google/libraries/SupportFor34.py
@@ -150,10 +150,10 @@ def f_ready_for_graph_int(i_arg1, i_arg2):
    print(i_arg1)
     
    df_PatientVisit = pd.DataFrame([
-      [ i_arg2   , str(i_arg1), "PatientVisit"],
-      [ "XX-1001", "XX-1001"  , "PatientVisit"],
-      [ "XX-1002", "XX-1002"  , "PatientVisit"],
-      [ "XX-1003", "XX-1003"  , "PatientVisit"],
+      [ i_arg2   , i_arg1              , "PatientVisit"],
+      [ "XX-1001", {"x-col": "XX-1001"}, "PatientVisit"],
+      [ "XX-1002", {"x-col": "XX-1002"}, "PatientVisit"],
+      [ "XX-1003", {"x-col": "XX-1003"}, "PatientVisit"],
          #
    ], columns = ["id", "transcription", "LABEL"])
       #
