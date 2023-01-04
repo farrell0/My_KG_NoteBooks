639970eb11159b1c90b8b670c81d9a9ace064b48
Push.sh <Push.sh@example.com>
2023-01-04 20:35:59 +0000

    No message

--- P1_Prospects/13_DataBricks_Google/libraries/SupportFor34.py
+++ P1_Prospects/13_DataBricks_Google/libraries/SupportFor34.py
@@ -272,16 +272,16 @@ def f_ready_for_graph_int(i_arg1, i_arg2):
                   #
                   #  If this key is present, add it to the dictionary
                   #
-                  if ("preferredTerm" in l_entity):
-                     #
-                     #  We have an additional key, add to the record and add to our record
-                     #
-                     l_recd1.update( {"preferred_term": [str(l_entity["preferredTerm"])]} )
-                  else:
-                     l_recd1.update( {"preferred_term": ["Unknown"                     ]} )
+#                 if ("preferredTerm" in l_entity):
+#                    #
+#                    #  We have an additional key, add to the record and add to our record
+#                    #
+#                    l_recd1.update( {"preferred_term": [str(l_entity["preferredTerm"])]} )
+#                 else:
+#                    l_recd1.update( {"preferred_term": ["Unknown"                     ]} )
                         #
                   #  df_UmlsEntityNodes.append(l_recd1, ignore_index = True)
-                  df_UmlsEntityNodes.append( pd.DataFrame(l_recd1))
+                  df_UmlsEntityNodes.append( pd.DataFrame(l_recd1) )
                 
                 
                 
@@ -431,8 +431,6 @@ def f_enrich(i_arg1, i_arg2):
    #  f_insert_into_graph(i_arg2, l_df1, l_df2, l_df3, l_df4, l_df5, l_df6, l_df7)
 
 
-   print(l_df1)
-   print("TTT")
    print(l_df2)
    print("TTT")
    print(l_df3)
