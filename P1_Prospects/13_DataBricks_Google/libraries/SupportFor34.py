3e29b4bf1161f057651c37eb37d1afb2e7c78280
Push.sh <Push.sh@example.com>
2023-01-05 00:46:28 +0000

    No message

--- P1_Prospects/13_DataBricks_Google/libraries/SupportFor34.py
+++ P1_Prospects/13_DataBricks_Google/libraries/SupportFor34.py
@@ -419,11 +419,7 @@ def f_enrich(i_arg1, i_arg2):
       #
    l_df1, l_df2, l_df3, l_df4, l_df5, l_df6, l_df7 = f_ready_for_graph_int(l_data_asjson, l_uniqid)
       #
-   #  f_insert_into_graph(i_arg2, l_df1, l_df2, l_df3, l_df4, l_df5, l_df6, l_df7)
+   f_insert_into_graph(i_arg2, l_df1, l_df2, l_df3, l_df4, l_df5, l_df6, l_df7)
 
-
-   print(l_df2)
-   print("TTT")
-   print(l_df3)
         
    return l_data_asjson, l_uniqid
